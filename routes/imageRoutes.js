65dc7e20898b7905540ed1b6cde9a56fa615d06e
gowthamazmeera09 <gowthamazmeera@gmail.com>
2024-11-23 09:23:07 +0530

    jifbvdfhbdfvbdfhdfv

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { upload, uploadImage, getAllImages } = require('../controller/imageController');
+const { upload, uploadImage, getAllImages } = require('../controller/imagecontroller');
 
 router.post('/upload', upload.single('image'), uploadImage);
 router.get('/images', getAllImages);
